9ad008485222cb1a1ec9f9a1b40cdcec2cf54c4a
Fabkar <2003@holbertonschool.com>
2020-09-01 17:43:09 +0000

    fixit issues with pep8

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -10,5 +10,5 @@ if x > 5:
 elif x == 0:
     print("last digit of {} is 0 and is 0".format(number))
 else:
-    print("last digit of {} is".format(number),end=' ')
+    print("last digit of {} is".format(number), end=' ')
     print("{} and is less than 6 and not 0".format(x))
