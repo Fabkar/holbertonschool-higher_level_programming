b636aef942f8d7c5333b1c2b2957c03fec989939
Fabkar <2003@holbertonschool.com>
2020-09-08 18:49:58 +0000

    fixit

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0 and idx > len(my_list):
-        return None
+    if idx > len(my_list):
+        return
+    if idx < 0:
+        return
     return my_list[idx]
