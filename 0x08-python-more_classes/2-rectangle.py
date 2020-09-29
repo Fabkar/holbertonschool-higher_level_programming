3b8e4f782d4fa4f05023902777dca07293b7b13e
Fabkar <2003@holbertonschool.com>
2020-09-29 01:40:22 +0000

    change condition line 73

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -70,6 +70,6 @@ class Rectangle:
         return self.height * self.width
 
     def perimeter(self):
-        if (self.width or self.height) == 0:
+        if self.width == 0  or self.height == 0:
             return 0
         return (2 * self.height) + (2 * self.width)
