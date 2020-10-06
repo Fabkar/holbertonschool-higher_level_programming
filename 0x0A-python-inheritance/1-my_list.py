b6ee8bf705f4fc489392847516e216fc37982b8d
Fabkar <2003@holbertonschool.com>
2020-10-06 23:34:59 +0000

    solve comments

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,8 +7,13 @@ My class list that inherits from list
 class MyList(list):
     """Subclass of list"""
     def __init__(self):
-        """ Initializace all instance of method"""
+        """
+        Initializace all instance of method
+        """
         super().__init__
-    """metod to print my list ascending sort"""
+
     def print_sorted(self):
+        """
+        method to print my list ascending sort
+        """
         print(sorted(self))
