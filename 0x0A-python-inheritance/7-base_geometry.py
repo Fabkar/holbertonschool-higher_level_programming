6a043191490716d4cadec807d9aa0b2216a09163
Fabkar <2003@holbertonschool.com>
2020-10-07 02:44:18 +0000

    fix pep8

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -9,6 +9,7 @@ class BaseGeometry:
         """area is not implemented
         """
         raise Exception("area() is not implemented")
+
     def integer_validator(self, name, value):
         """Integer Validator
         """
