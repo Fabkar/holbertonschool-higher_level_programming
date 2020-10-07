fedb951d928e0f0aade8e60a94c90c4abaf57579
Fabkar <2003@holbertonschool.com>
2020-10-07 22:33:31 +0000

    Script that adds all arguments

---
+++ 0x0B-python-input_output/9-add_item.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+script that adds all arguments to a Python list
+"""
+import sys
+
+
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
+file = "add_item.json"
+try:
+    new_file = load_from_json_file(file)
+except (FileExistsError, FileNotFoundError, ValueError):
+    new_file = []
+for arg in sys.argv[1:]:
+    new_file.append(arg)
+save_to_json_file(new_file, file)
