193f9e52e0e04217d1f660e9ea9c109b1464f7b7
Fabkar <2003@holbertonschool.com>
2020-10-07 22:20:08 +0000

    fix indentation

--- 0x0B-python-input_output/6-from_json_string.py
+++ 0x0B-python-input_output/6-from_json_string.py
@@ -6,7 +6,7 @@ import json
 
 
 def from_json_string(my_str):
-	"""
-	return an object represented by a JSON string
-	"""
-	return json.loads(my_str)
+    """
+    return an object represented by a JSON string
+    """
+    return json.loads(my_str)
