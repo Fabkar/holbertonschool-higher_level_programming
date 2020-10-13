d11eea54fe5ab1939de459349733d705a7b0f9d4
Fabkar <2003@holbertonschool.com>
2020-10-13 23:30:25 +0000

    update

---
+++ 0x0C-python-almost_a_circle/tests/test_models/test_square.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Unitest for model/base.py
+"""
+
+import pep8
+import unittest
+import inspect
+from models.base import Base
+from models.square import Square, __doc__ as mrdoc
+
+
+class TestSquare(unittest. TestCase):
+    """
+    Test to Square Class
+    """
+    def test_docstring(self):
+        """ check each base class with docstrings"""
+        self.assertTrue(len(mrdoc.strip()) > 0)
+        self.assertTrue(len(Base.__doc__.strip()) > 0)
+        functions = inspect.getmembers(Base, predicate=inspect.ismethod)
+        for name, func in functions:
+            self.assertTrue(len(func.__doc__.strip()) > 0)
+        functions = inspect.getmembers(Base, predicate=inspect.isfunction)
+        for name, func in functions:
+            self.assertTrue(len(func.__doc__.strip()) > 0)
+
+    def test_pep8(self):
+        """check Test to pep8"""
+        pep8style = pep8.StyleGuide(quiet=True)
+        result = pep8style.check_files(["models/rectangle.py"])
+        self.assertEqual(result.total_errors, 0)
+
+    def test_square_correct(self):
+        """Checking each attribute of square"""
+        r = Square(2)
+        self.assertEqual(r.size, 2)
+        self.assertEqual(r.x, 0)
+        self.assertEqual(r.y, 0)
+        self.assertEqual(r.id, 3)
