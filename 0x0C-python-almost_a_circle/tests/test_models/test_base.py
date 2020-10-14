f91a471e108a88a36c5a9f7dc6acc47440d19ee2
Fabkar <2003@holbertonschool.com>
2020-10-14 00:25:23 +0000

    adding more case

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -29,11 +29,11 @@ class TestBase(unittest.TestCase):
         1. Base Class
         """
         b = Base()
-        self.assertEqual(b.id, 1)
-        self.assertEqual(Base._Base__nb_objects, 1)
+        self.assertEqual(b.id, 8)
+        self.assertEqual(Base._Base__nb_objects, 8)
         b2 = Base(3)
         self.assertEqual(b2.id, 3)
-        self.assertEqual(Base._Base__nb_objects, 1)
+        self.assertEqual(Base._Base__nb_objects, 8)
         b2.id = 4
         self.assertEqual(b2.id, 4)
         b2.id = "Test"
@@ -81,6 +81,73 @@ class TestBase_to_json_string(unittest.TestCase):
 
     def test_to_json_string_none(self):
         self.assertEqual("[]", Base.to_json_string(None))
+
+
+class Base_test_class(unittest.TestCase):
+    """
+    Unittest for testing instantiation
+    """
+    def test_base(self):
+        """ test base """
+        b = Base(1)
+        self.assertEqual(b.id, 1)
+        b.id = 9
+        self.assertEqual(b.id, 9)
+
+    def test_without_arg(self):
+        """ test without arguments """
+        self.assertEqual(Base().id, (Base().id) - 1)
+
+    def test_more_arg(self):
+        """ test more arguments (3) """
+        b1, b2, b3 = Base(), Base(), Base()
+        self.assertEqual(b1.id, b3.id - 2)
+
+    def test_nb_instance(self):
+        """ test numb instance """
+        b1, b2, b3 = Base(), Base(1), Base()
+        self.assertEqual(b1.id, b3.id - 1)
+
+    def test_id(self):
+        """ tesd id """
+        self.assertEqual(5, Base(5).id)
+        b, b.id = Base(10), 5
+        self.assertEqual(5, b.id)
+
+    def test_bool_id(self):
+        """ test boolean id pass """
+        self.assertEqual(True, Base(True).id)
+
+    def test_base_tuple_id(self):
+        """ test tuple id """
+        self.assertEqual((1, 2), Base((1, 2)).id)
+
+    def test_num_id(self):
+        self.assertEqual(range(9), Base(range(9)).id)
+
+    def test_base_dict(self):
+        self.assertEqual({'a': 1, 'b': 2}, Base({'a': 1, 'b': 2}).id)
+
+    def test_set_id(self):
+        self.assertEqual({1, 2, 3}, Base({1, 2, 3}).id)
+
+    def test_base_NaN_id(self):
+        self.assertNotEqual(float('NaN'), Base(float('NaN')).id)
+
+    def test_two_args(self):
+        with self.assertRaises(TypeError):
+            Base(1, 2)
+
+    def test_list_id(self):
+        self.assertEqual({'a': 1, 'b': 2}, Base({'a': 1, 'b': 2}).id)
+
+    def test_docstring(self):
+        self.assertTrue(len(Base.__doc__.strip()) > 0)
+        self.assertTrue(len(Base.to_json_string.__doc__.strip()) > 0)
+        self.assertTrue(len(Base.save_to_file.__doc__.strip()) > 0)
+        self.assertTrue(len(Base.from_json_string.__doc__.strip()) > 0)
+        self.assertTrue(len(Base.create.__doc__.strip()) > 0)
+        self.assertTrue(len(Base.load_from_file.__doc__.strip()) > 0)
 
 if __name__ == '__main__':
     unittest.main()
