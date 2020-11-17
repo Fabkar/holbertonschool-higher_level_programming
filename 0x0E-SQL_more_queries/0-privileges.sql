d9ddac38b30a0c4fa8543494027e2238d5173271
Fabkar <2003@holbertonschool.com>
2020-11-17 12:17:42 -0500

    update

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
 -- lists all privileges of the MySQL user user_od_1 and user_0d_2 on your server
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
+-- list all privileges to user
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
