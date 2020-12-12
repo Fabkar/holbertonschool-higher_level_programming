61b0e2d270a308ee753ab36afe0451558e17e64b
Fabkar <2003@holbertonschool.com>
2020-12-12 14:03:10 -0500

    solve line long

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -10,6 +10,7 @@ if __name__ == "__main__":
     c = database.cursor()
     c.execute("SELECT cities.id, cities.name, states.name "
               "FROM cities "
-              "JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC ")
+              "JOIN states ON cities.state_id = states.id "
+              "ORDER BY cities.id ASC ")
     for states in c.fetchall():
         print(states)
