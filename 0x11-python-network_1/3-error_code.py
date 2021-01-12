55afb88b0c7d421841bd31f8b20fe755adecba3c
Fabkar <2003@holbertonschool.com>
2021-01-11 19:52:47 -0500

    update

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -12,4 +12,4 @@ if __name__ == "__main__":
         with request.urlopen(req) as response:
             print(response.read().decode("utf-8"))
     except error.HTTPError as error:
-        print("Error code : {}".format(error.code))
+        print("Error code: {}".format(error.code))
