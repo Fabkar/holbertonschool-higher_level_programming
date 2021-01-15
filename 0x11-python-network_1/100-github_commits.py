6c034cbfe75ddcd45a057d45f9ffa13e0bb47fbb
Fabkar <2003@holbertonschool.com>
2021-01-15 11:24:16 -0500

    some changes

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -6,9 +6,10 @@ from sys import argv
 if __name__ == "__main__":
     repo = argv[1]
     owner = argv[2]
+    headers = {'Accept': 'application/vnd.github.v3+json'}
     # "https://docs.github.com/en/free-pro-team@latest/rest/reference/users"
     URL = "https://api.github.com/repos/{}/{}/commits".format(owner, repo)
-    req = requests.get(URL)
+    req = requests.get(URL, headers=headers)
     json = req.json()
     try:
         for i in range(10):
