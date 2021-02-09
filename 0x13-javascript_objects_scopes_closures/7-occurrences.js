ced4278cb960855bc5b2e950a5408dc53780122a
Fabkar <fab_862@hotmail.com>
2021-02-08 23:10:46 -0500

    looking for a occurrences in a lists

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,8 +1,8 @@
-#!/usr/bin/ node
+#!/usr/bin/node
 /* function that returns the number of occurrences in a list */
 exports.nbOccurences = function (list, searchElement) {
   let clons = 0;
-  for (let i = 0; i < list.length; i++) { // check all list
+  for (let i = 0; i < list.length; i++) { // checks all list
     if (list[i] === searchElement) clons += 1; // fetching the clon
   }
   return (clons);
