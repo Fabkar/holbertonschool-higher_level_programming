2c2c56844d95d04d82b46e78546e0277d855397d
Fabkar <fab_862@hotmail.com>
2021-02-08 22:32:08 -0500

    new method square inherits from other class

---
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -0,0 +1,34 @@
+#!/usr/bin/node
+/* class Rectangle that defines a rectangle */
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) { // Conditional when the sides are greater than 0
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) { // for each space print an X with the size of rectangle
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  rotate () { // that exchanges the width and the height of the rectangle
+    const i = this.width;
+    this.width = this.height;
+    this.height = i;
+  }
+
+  double () { // that multiples the width and the height of the rectangle by 2.
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+};
+
+const Rectangle = require('./4-rectangle');
+module.exports = class Square extends Rectangle { // class square inherits from 4-rectangle
+  constructor (size) {
+    super(size, size);
+  }
+};
