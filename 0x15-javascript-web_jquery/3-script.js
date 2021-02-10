ed1ae3cdb4277bc2dade7ff81a1ed092dfb2bb7b
Fabkar <fab_862@hotmail.com>
2021-02-10 11:43:39 -0500

    add a class red

---
+++ 0x15-javascript-web_jquery/3-script.js
@@ -0,0 +1,5 @@
+/**Javascript script that adds the class red to the HTML
+ * tag HEADER when the user clicks on the tag DIV#red_header */
+$('#red_header').click(function () {
+  $('HEADER').addClass('red');
+});
